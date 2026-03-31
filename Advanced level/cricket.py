79c0ee43f50713ace94c9872ce4a0c886db2bfc1
yeturuaswini <yeturuaswini2@gmail.com>
2026-03-31 13:19:48 +0530

    Add files via upload

---
+++ Advanced level/cricket.py
@@ -0,0 +1,12 @@
+runs = [45, 67, 89, 120, 34]
+
+total_runs = sum(runs)
+average_runs = total_runs / len(runs)
+highest_score = max(runs)
+lowest_score = min(runs)
+
+print("Runs List:", runs)
+print("Total Runs:", total_runs)
+print("Average Runs:", average_runs)
+print("Highest Score:", highest_score)
+print("Lowest Score:", lowest_score)
